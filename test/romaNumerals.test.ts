2f57f6fd02287f66e9d12b213521ef8ab4bb03e1
Steven Borrie <saborrie@gmail.com>
2026-02-09 09:45:00 +0000

    first test

--- test/romaNumerals.test.ts
+++ test/romaNumerals.test.ts
@@ -1,5 +1,5 @@
-import {expect, test} from 'vitest';
+import { expect, test } from "vitest";
 
-test('Empty test', () => {
-  expect(true).toBe(true);
+test("1 is I", () => {
+  expect(romanNumeralOf(1)).toBe("I");
 });
