631e1ec2fa6c50c85da47ea488971dd8113c4558
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2026-02-10 10:47:23 +0100

    Implement all the tens

--- src/formatRomanNumeral.ts
+++ src/formatRomanNumeral.ts
@@ -1,9 +1,9 @@
 export function formatRomanNumeral(number: number): string {
-  if (number == 10) {
-    return "X";
-  }
-  if (number == 50) {
-    return "L";
+  if (number >= 10) {
+    return formatRomanNumeral(Math.floor(number / 10))
+      .replaceAll('X', 'C')
+      .replaceAll('I', 'X')
+      .replaceAll('V', 'L');
   }
   const expected = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
   return expected[number];
